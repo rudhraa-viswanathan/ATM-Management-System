0980b4db400a050fa2073ae9d5707b5e3b832bf3
Rudhraa Viswanathan <rudhraaviswanathan2@gmail.com>
2026-07-15 13:39:03 +0530

    Initial Maven project setup

---
+++ src/main/java/com/rudhraa/atm/ATMApplication.java
@@ -0,0 +1,13 @@
+package com.rudhraa.atm;
+
+public class ATMApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println("      ATM MANAGEMENT SYSTEM");
+        System.out.println("==================================");
+        System.out.println("Application Started...");
+
+    }
+}
